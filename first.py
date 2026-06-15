82b2c2665945094ee2e77d75eae694727af9e9cf
467010-ai <467010@wsk.ac.th>
2026-06-15 02:28:59 +0000

    first.py

--- first.py
+++ first.py
@@ -1,1 +1,9 @@
-print("hello world")
+width = float(input("Enter the width: "))
+length = float(input("Enter the length: "))
+height = float(input("Enter the height: "))
+
+area = width * length
+volume = width * length * height
+
+print(f"Area of the rectangle = {area} sq units")
+print(f"Volume of the rectangular prism = {volume} cu units")
